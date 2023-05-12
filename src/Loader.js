cda3d6e976f08f189e3a4aed8c65851760dc97cd
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-13 01:47:55 +0530

    Update Loader.js

--- src/Loader.js
+++ src/Loader.js
@@ -4,7 +4,7 @@ import React from "react";
 function Loader() {
   return (
     <div className="flex items-center justify-center bg-black h-[100vh]">
-      <div className="overlay"></div>
+     
 
       <video playsInline loop autoPlay muted src='assets/loader.webm' />
 
