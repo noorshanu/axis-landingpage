493fe698556b7fe0ec4b6382f95485109a5e8baf
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-12 10:58:20 +0530

    new

--- src/sections/Team.js
+++ src/sections/Team.js
@@ -4,21 +4,18 @@ import TeamCard from "../components/TeamCard";
 let teams = [];
 const Team = () => {
   return (
-    <div>
-      <div className="mt-24 grid grid-cols-2 md:grid-cols-4 gap-5">
-        <div className=" col-span-2  py-5 ">
-          <h1 className="capitalize text-4xl mb-3 text-[#11FF37] font-monument">
+    <div className="mb-24" >
+      <div className="mt-24 ">
+        <div className=" col-span-2  py-2 ">
+          <h1 className="capitalize text-4xl mb-3 text-[#6BFF83] font-monument text-center">
             our team
           </h1>
-          <p className="text-[#9AA0A6] text-lg font-red-hat">
-            Dozens of experienced individuals are working on the success of
-            Axis. Meet them all!
-          </p>
+      
         </div>
-        <TeamCard />
-        <TeamCard />
+        
+       
       </div>
-      <div className="grid sm:grid-cols-2 grid-cols-1  md:grid-cols-4 gap-7 mt-7">
+      {/* <div className="grid sm:grid-cols-2 grid-cols-1  md:grid-cols-4 gap-7 mt-7">
         <TeamCard className={"border-t border-l-0 "} />
         <TeamCard className={"border-t border-l-0 "} />
         <TeamCard className={"border-t border-l-0 "} />
@@ -27,6 +24,11 @@ const Team = () => {
         <TeamCard className={"border-t border-l-0 "} />
         <TeamCard className={"border-t border-l-0 "} />
         <TeamCard className={"border-t"} />
+      </div> */}
+
+      <div className="m-auto text-center">
+        <img src="assets/images/team.png" alt="" className="m-auto"/>
+
       </div>
     </div>
   );
--- src/sections/road2.js
+++ src/sections/road2.js
@@ -3,16 +3,11 @@ import React from "react";
 const Road = () => {
   return (
     <>
-      <h1 className=" mt-32 mb-12 font-monument text-[#ACFFB9] text-2xl font-bold sm:text-3xl text-center mx-auto">
-        Axis Roadmap
+      <h1 className=" mt-32 mb-12 font-monument text-[#6BFF83] text-2xl font-bold sm:text-3xl text-center mx-auto">
+        Roadmap
       </h1>
       <div className="flex relative text-center">
         <img src="assets/images/roadmap.svg" alt=""/>
-      
-        
-
-
-       
       </div>
     </>
   );
