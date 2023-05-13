05d1b7a6350cf15bd2a6f63c4b695ecf43153dcc
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-13 14:12:33 +0530

    jj

--- src/sections/Mission.js
+++ src/sections/Mission.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation, Pagination, EffectCoverflow } from "swiper";
+import SwiperCore, {  Pagination, EffectCoverflow, Autoplay } from "swiper";
 
 // Import Swiper styles
 import "swiper/swiper.scss";
@@ -8,17 +8,24 @@ import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
 import "swiper/components/effect-coverflow/effect-coverflow.scss";
 
-SwiperCore.use([Navigation, Pagination, EffectCoverflow]);
+SwiperCore.use([Autoplay,Pagination, EffectCoverflow]);
 
 
 const Mission = () => {
   return (
     <>
     
-    <div className="App">
+    <div className="App  mt-16 mb-6 sm:mt-20 ">
       <Swiper
-        navigation
-        pagination={{ clickable: true }}
+      loop={true}
+      speed= {1400}
+      autoplay={{
+        delay: 1,
+        disableOnInteraction: true,
+        // reverseDirection:true,
+
+      }}
+      initialSlide={3}
         effect="coverflow"
         coverflowEffect={{
           rotate: 50,
@@ -27,98 +34,49 @@ const Mission = () => {
           modifier: 1,
           slideShadows: false
         }}
-        slidesPerView={2}
-        centeredSlides
+        slidesPerView={4}
+        centeredSlides={true}
         style={{ height: "500px" }}
       >
         <SwiperSlide
-          style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-1.jpg)"
-          }}
         >
-          Slide 1
+          <img src="assets/images/main.png" alt=""/>
         </SwiperSlide>
         <SwiperSlide
-          style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-2.jpg)"
-          }}
+        
         >
-          Slide 2
+           <img src="assets/images/main.png" alt=""/>
         </SwiperSlide>
         <SwiperSlide
-          style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-3.jpg)"
-          }}
+         
         >
-          Slide 3
+         <img src="assets/images/main.png" alt=""/>
         </SwiperSlide>
         <SwiperSlide
-          style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-4.jpg)"
-          }}
+        
         >
-          Slide 4
+           <img src="assets/images/main.png" alt=""/>
         </SwiperSlide>
         <SwiperSlide
-          style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-5.jpg)"
-          }}
+        
         >
-          Slide 5
+           <img src="assets/images/main.png" alt=""/>
         </SwiperSlide>
         <SwiperSlide
-          style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-6.jpg)"
-          }}
+       
         >
-          Slide 6
+           <img src="assets/images/main.png" alt=""/>
         </SwiperSlide>
-        <SwiperSlide
-          style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-7.jpg)"
-          }}
-        >
-          Slide 7
-        </SwiperSlide>
-        <SwiperSlide
-          style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-8.jpg)"
-          }}
-        >
-          Slide 8
-        </SwiperSlide>
-        <SwiperSlide
-          style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-9.jpg)"
-          }}
-        >
-          Slide 9
-        </SwiperSlide>
-        <SwiperSlide
-          style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-10.jpg)"
-          }}
-        >
-          Slide 10
-        </SwiperSlide>
+
+     
+    
+     
       </Swiper>
     </div>
   
 
     <div className="  mt-16 sm:mt-20 ">
-      <div className="mx-auto">
-      <img src="assets/images/phone.png" alt=""/>
-      </div>
+      
   
   <div className="mx-auto sub-info relative">
     <img src="assets/images/whiteLine.svg" className="left-0 absolute" alt=""/>
