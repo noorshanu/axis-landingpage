03b85ec94df78c684563c766305f6f76ea0047a0
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-13 13:14:38 +0530

    new

--- src/sections/Mission.js
+++ src/sections/Mission.js
@@ -7,7 +7,9 @@ const Mission = () => {
       <img src="assets/images/phone.png" alt=""/>
       </div>
   
-  <div className="mx-auto sub-info">
+  <div className="mx-auto sub-info relative">
+    <img src="assets/images/whiteLine.svg" className="left-0 absolute" alt=""/>
+
     <div className="border-right">
       <h1 className="text-[#6BFF83] font-monument font[400] text-3xl">2650</h1>
       <p className="text-[#fff] font-red-hat font-[500] text-xl mt-6">Beta testers</p>
@@ -25,7 +27,7 @@ const Mission = () => {
       <p className="text-[#fff] font-red-hat font-[500] text-xl mt-6">Beta testers</p>
 
     </div>
-     
+    <img src="assets/images/whiteLine.svg" alt="" className="right-0 absolute"/>
   </div>
     </div>
   );
