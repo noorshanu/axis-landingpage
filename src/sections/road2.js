e43778f1b5958bf21a22e08e867c8ac7547cb0cb
unknown <anas.452111@gmail.com>
2023-05-24 17:18:05 +0500

    minor bug fixed

--- src/sections/road2.js
+++ src/sections/road2.js
@@ -7,7 +7,7 @@ const Road = () => {
       <h1 className=" mt-32 mb-12 font-monument text-[#6BFF83] text-2xl font-bold sm:text-3xl text-center mx-auto">
         Roadmap
       </h1>
-      <div className="hidden top-[20%] top-[61%] max-w-[12%] max-w-[15%] text-[1vw] text-[0.8vw] left-[3%] left-[34%] left-[17%] left-[67%] left-[53%] left-[86%]" />
+      <div className="hidden top-[20%] top-[10%] top-[61%] max-w-[12%] max-w-[15%] text-[1vw] text-[0.8vw] left-[3%] left-[34%] left-[17%] left-[67%] left-[53%] left-[86%]" />
 
       <div className="relative bg-[url('../public/assets/images/RoadmapFrame.svg')] bg-cover">
         <img
